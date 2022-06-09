86583de66b86c097ebbf0405defd0ed68b28c174
dancruznyc <dancruznyc@gmail.com>
2022-06-09 13:17:41 -0400

    choosing colors

---
+++ src/Pages/Admin/Admin.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Admin.css";
+
+const Admin = () => {
+  return (
+    <div>
+      <main className="admin--container">
+        <h1>hello</h1>
+        <h2>hello</h2>
+        <h3>hello</h3>
+        <h4>hello</h4>
+        <h5>hello</h5>
+      </main>
+    </div>
+  );
+};
+
+export default Admin;
