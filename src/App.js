cc0c03975f1da7d1563b098db692189acdcc2e4a
dancruznyc <dancruznyc@gmail.com>
2022-06-23 20:37:45 -0400

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import Admin from "./Pages/Admin/Admin";
+import Home from "./Pages/Home/Home";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
+  useEffect(() => {
+    fetch();
+  }, []);
   return (
     <div className="App">
-      <Admin />
+      <Sidebar />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <ul className="sidebar--container">
+      <li>MixologyBuddy</li>
+      <li>My Bar</li>
+      <li>Favorites</li>
+      <li>My Recipes</li>
+      <li>All Recipes</li>
+      <li>News</li>
+      <li>Sales</li>
+    </ul>
+  );
+};
+
+export default Sidebar;
