ed05e92dfe8a58c96cfb858daad14cec2c9c8469
dancruznyc <dancruznyc@gmail.com>
2022-06-25 19:27:43 -0400

    sidebar edits and packages

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./Sidebar.css";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <ul className="sidebar--container">
-      <li>MixologyBuddy</li>
-      <li>My Bar</li>
-      <li>Favorites</li>
-      <li>My Recipes</li>
-      <li>All Recipes</li>
+      <li>
+        <Link to="/">MixologyBuddy</Link>
+      </li>
+      <li>
+        <Link to="/mybar">My Bar</Link>
+      </li>
+      <li>
+        {" "}
+        <Link to="/favorites">Favorites</Link>
+      </li>
+      <li>
+        <Link to="/myrecipes">My Recipes</Link>
+      </li>
+      <li>
+        <Link to="/allrecipes">All Recipes</Link>
+      </li>
       <li>News</li>
       <li>Sales</li>
     </ul>
