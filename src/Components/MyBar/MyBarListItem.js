a21ce22f0c3fea3eda96426e383bbf79acb7b4d6
Daniel Delacruz <dancruznyc@gmail.com>
2022-08-22 11:16:31 -0400

    added local storage for mybarlist

--- src/Components/MyBar/MyBarListItem.js
+++ src/Components/MyBar/MyBarListItem.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import "./MyBarListItem.css";
+import { BsThreeDotsVertical, BsTrashFill } from "react-icons/bs";
+import { GrEdit } from "react-icons/gr";
+import { IoMdCloseCircle } from "react-icons/io";
+import { MyBarContext } from "../../Services/MyBarContext";
 
 const MyBarListItem = ({ id, name, brand }) => {
+  const [toggle, setToggle] = useState(false);
+  function toggleAccordian() {
+    setToggle(!toggle);
+  }
+
+  const { removeDrink } = useContext(MyBarContext);
   return (
     <div className="mybar-list--item">
-      <p className="name-par">{name}</p>
-      <p className="brand-par">{brand}</p>
-      <button>Remove</button>
-      <button>Edit</button>
+      <div className="list-item-content--container">
+        <div className="list-item--content">
+          <p className="name-par">{name}</p>
+          <p className="brand-par">{brand}</p>
+        </div>
+        <div className="open-accordian-btn">
+          {toggle === false ? (
+            <BsThreeDotsVertical onClick={toggleAccordian} />
+          ) : (
+            <IoMdCloseCircle onClick={toggleAccordian} />
+          )}
+        </div>
+      </div>
+      <div className={toggle === false ? "control-btns" : "control-btns show"}>
+        <button>
+          <BsTrashFill onClick={() => removeDrink(id)} />
+        </button>
+        <button>
+          <GrEdit />
+        </button>
+      </div>
     </div>
   );
 };
--- src/Services/MyBarContext.js
+++ src/Services/MyBarContext.js
@@ -1,18 +1,34 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { v4 } from "uuid";
 
 export const MyBarContext = createContext();
 
 export const MyBarContextProvider = ({ children }) => {
   const [myDrinksList, setMyDrinksList] = useState([]);
-  function myBarLoader() {}
+  function myBarLoader() {
+    const list = JSON.parse(localStorage.getItem("myBarList"));
+    if (list) {
+      setMyDrinksList(list);
+    }
+  }
   function addNewDrink(drinkName, drinkBrand) {
     const id = v4();
-    setMyDrinksList((prev) => {
-      return [...prev, { id: id, name: drinkName, brand: drinkBrand }];
-    });
+    const newList = [
+      ...myDrinksList,
+      { id: id, name: drinkName, brand: drinkBrand },
+    ];
+    setMyDrinksList(newList);
+    localStorage.setItem("myBarList", JSON.stringify(newList));
   }
-  function removeDrink() {}
+  function removeDrink(id) {
+    const newList = myDrinksList.filter((item) => item.id !== id);
+    setMyDrinksList(newList);
+    localStorage.setItem("myBarList", JSON.stringify(newList));
+  }
+
+  useEffect(() => {
+    myBarLoader();
+  }, []);
 
   return (
     <MyBarContext.Provider
