cd2fc97aa1805a5086bf92dea8d8f83918ec73b2
Daniel Delacruz <dancruznyc@gmail.com>
2022-09-05 17:04:22 -0400

    mybar instructions

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,47 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
+import {
+  AiFillAndroid,
+  AiFillApple,
+  AiOutlineTwitter,
+  AiFillFacebook,
+  AiFillInstagram,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
     <section className="footer--container">
-      <aside className="footer--left"></aside>
-      <div className="footer-main"></div>
+      <ul className="footer-links">
+        <li className="li footer-link--item">
+          <Link to={"/about"}>About</Link>
+        </li>
+        <li className="li footer-link--item">
+          <Link to={"/help"}> Help</Link>
+        </li>
+
+        <li className="li footer-link--item">
+          <Link to={"/agreement"}>User Agreement</Link>
+        </li>
+
+        <li className="li footer-link--item">
+          <Link to={"/privacy"}> Privacy</Link>
+        </li>
+        <li className="li footer-link--item">
+          <Link to={"/"}> Mixology Buddy &copy; 2023</Link>
+        </li>
+      </ul>
+      <div className="footer-icons">
+        <div className="mobile-icons">
+          <AiFillAndroid />
+          <AiFillApple />
+        </div>
+        <div className="social-icons">
+          <AiOutlineTwitter />
+          <AiFillFacebook />
+          <AiFillInstagram />
+        </div>
+      </div>
     </section>
   );
 };
--- src/Components/MyBar/MyBarInstructions.js
+++ src/Components/MyBar/MyBarInstructions.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { GiGlassShot } from "react-icons/gi";
+import "./MyBarInstructions.css";
 
 const MyBarInstructions = () => {
-  return <div className="mybar-instructions--container">MyBarInstructions</div>;
+  return (
+    <ul className="mybar-instructions--container">
+      <h3 className="mybar-instructions--title">Tips</h3>
+      <li className="mybar-instructions--item">
+        <GiGlassShot />
+        <div>
+          Include all non-alcoholic drinks you have. eg. Orange Juice, Milk,
+          Water
+        </div>
+      </li>
+      <li className="mybar-instructions--item">
+        <GiGlassShot />
+        <div>Include all dry goods you have. eg. Salt, Pepper, Eggs</div>
+      </li>
+      <li className="mybar-instructions--item">
+        <GiGlassShot />
+        <div>Include all dry goods you have. eg. Salt, Pepper, Eggs</div>
+      </li>
+    </ul>
+  );
 };
 
 export default MyBarInstructions;
